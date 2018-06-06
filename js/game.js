faf76ef6574b693079bb22e04c2977b67058529e
tektechnologies <info@tektechnologies.com>
2018-06-06 08:53:42 -0500

    add project files for initial commit

---
+++ js/game.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var answer = prompt('Are you a Javascript Developer?')
+console.log('Is our guest a developer? = ' + answer);
+
+
+
+var codeLang = prompt('What\'s your favorite code language?');
+console.log('First Name = ' + codeLang);
+var lastName = prompt('What\'s is your Last Name?');
+console.log('Last Name = ' + lastName);
+var gameName = prompt('What\'s is your Favorite Game?');
+console.log('Favorite Game = ' + gameName);
+var systemName = prompt('What\'s is your Game Console?');
+
+  
+
+
+
+
+
+if(answer.toUpperCase() === "Y" || answer.toUpperCase() "YES"){
+         alert('Yes!, your on your way to having a great Career.');
+       } 
+       else if(answer === 'N'){
+         alert('No!');
+       } 
+       else {
+         alert('I did not get good response');
+       }
+
+
+
+
+
+       alert('Welcome ' + firstName + ' ' + lastName + ', ' + 'I too play, ' + gameName + ' on a  ' + systemName);
