af6d7a9cbdba7de70c3fc3fb8f073bcf687bf9c7
tektechnologies <info@tektechnologies.com>
2018-06-07 19:40:18 -0500

    this is the first entry for the third lab, guess number done

--- js/game.js
+++ js/game.js
@@ -74,46 +74,85 @@ if(answerFive.toUpperCase().trim() === 'YES' || answerFive.toUpperCase().trim() 
 }
 
 
-
+/*
 while (guess !== '2') {
- 
-  +  // guess will be null if they hit Cancel
+
+    // guess will be null if they hit Cancel
      if (guess === null) {
-  -    console.log('Guess is null; breaking');
-  +    console.log('User hit cancel for favorite number');
-   
+      console.log('Guess is null; breaking');
+      console.log('User hit cancel for favorite number');
+
        // Escape the loop!
        break;
   while (guess !== '2') {
      guessCount = guessCount + 1;
-   
+
      console.log('guessCount is ' + guessCount);
-  -  console.log('end of while loop');
-  +  console.log('about to return to top of while loop');
+    console.log('end of while loop');
+    console.log('about to return to top of while loop');
    }
-   
+
   -console.log('while loop exited');
   +console.log('done guessing (while loop is finished)');
-   
-  +// If they cancelled the prompt...
+
+  // If they cancelled the prompt...
    if (guess === null) {
-  +
+
      alert('Wimp. My favorite number is 2, obviously.');
-  +
+
    } else {
-  +  // They must have gotten the answer right
+    // They must have gotten the answer right
      console.log('not a wimp');
-   
+
      if (guessCount === 1) {
-  -    alert('Right on the first try!'); 
-  +    alert('Right on the first try!');
+  -    alert('Right on the first try!');
+      alert('Right on the first try!');
      } else {
        alert('It took ' + guessCount + ' guesses to get it right!');
-     }
-  
+     }*/
 
+/*
+   logic and structure from:
+   http://davidbau.com/javascript/learn/04-guess.html
+   I did go through entire code and rewrite it.
+   */
 
+var y = Math.floor(Math.random() * 10) + 1;
+var opportunities = 4;
+var robotVoice = 'Guess a integer from 1 to 10!';
 
+/*beggining of while loop checks the condition to see if there are any turns left. */
+while (opportunities > 0) {
+  /*Lets the user know to guess an integer and how many chances are left.*/
+  var userChoice = prompt(robotVoice + ' You have ' + opportunities + ' try\'s to go.');
+  /*Lets the out of the program if they select cancel.*/
+  if (!userChoice){
+    break;
+  } 
+  /**The Number() function converts the object
+   argument to a number that represents the object's value. */
+  userChoice = Number(userChoice);
+  /**check condition of input to random number selected */
+  if (userChoice === y) {
+    /**I need help with this part I know I need it I just dont know why. */
+    opportunities = 0;
+  } else {
+    /*Notfies user that they were wrong and take the input decides if 
+    * it was higher or lower than the random number in the if statements. */
+    robotVoice = 'You were close, but no, try again.';
+    if (userChoice < y){
+      robotVoice += ' Too Low!';
+    } 
+    if (userChoice > y) {
+      robotVoice += ' Too High!';
+    }
+    /*This decrements the opportunities each time the user enters a try. */
+    opportunities = opportunities - 1;
+  }
+}
+/*This line of code sits outside the loop so that if the user cancels or fails
+* to guess correct, then they are told what the answer is. */
+alert('The Random number Generator choose ' + y + ' as it\'s number .');
 
 
 
