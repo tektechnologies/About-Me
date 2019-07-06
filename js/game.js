e07bf93c7ee341fd7ca9b387c5a29b0b2990d68a
tektechnologies <info@tektechnologies.com>
2019-07-06 11:10:49 -0500

    add updates to code

--- js/game.js
+++ js/game.js
@@ -1,15 +1,13 @@
 'use strict';
 
-console.log('Working');
-
-
+console.log('js doc linked');
 
 startGame();
 
 function startGame(){
-
   var userName;
   userName =prompt('Hello, what is your name?');
+  
   console.log('name? ' + userName);
   alert('Hello ' + userName + ' thanks for visiting my site! and .......');
   questionOne();
@@ -21,107 +19,74 @@ function startGame(){
   questionSeven();
 }
 
-
-
 function questionOne(){
-
   alert('Welcome to the long awaited expansion that is the AboutMe Game.');
 
-  var questionOne = prompt('Do I have two Sons that are aged 17 and 22');
-
+  var questionOne = prompt('Do I have two Sons that are aged 18 and 24');
   var answerOne = questionOne.toUpperCase().trim();
 
   console.log(answerOne);
 
-  if(answerOne === 'YES' || answerOne === 'Y')
-  {
+  if(answerOne === 'YES' || answerOne === 'Y') {
     alert('That is Correct, I do have two sons, who are that old.');
-
-  } else if(answerOne === 'NO' || answerOne === 'N')
-  {
+  } else if(answerOne === 'NO' || answerOne === 'N') {
     alert('That is inCorrect, I am actually that old, Yikes!');
-
-  } else {
+  } else { 
     alert('Please remember to enter: yes  or  no!');
   }
 }
 
-
-
-function questionTwo(){
-
+function questionTwo() {
   var questionTwo = prompt('Is pepperoni pizza my favorite kind of ZA?');
   var answerTwo = questionTwo.toUpperCase().trim();
   console.log(answerTwo);
 
-
-  if(answerTwo === 'YES' || answerTwo === 'Y')
-  {
-    alert('That is Correct, Pepperonni is my Favorite ZA, Tamasos Detriot, to be exact.');
-
-
-  } else if(answerTwo === 'NO' || answerTwo === 'N')
-  {
+  if(answerTwo === 'YES' || answerTwo === 'Y') {
+    alert('That is Correct, Pepperonni is my Favorite ZA, Tamasos Detroit, to be exact.');
+  } else if(answerTwo === 'NO' || answerTwo === 'N') {
     alert('That is inCorrect, pepperonni for life, unitl my stomach can\'t take it anymore, anyway.');
-
   } else {
     alert('Please remember to enter: yes  or  no!');
   }
 }
 
 
-function questionThree(){
-
+function questionThree() {
   var questionThree = prompt('Have I owned an Atarri 2600, PS1, PS2, Xbox, Xbox360, XboxOne, and Nintendo?');
   var answerThree = questionThree.toUpperCase().trim();
   console.log(answerThree);
 
-  if(answerThree === 'YES' || answerThree === 'Y')
-  {
+  if(answerThree === 'YES' || answerThree === 'Y') {
     alert('That is Correct, I once played GTA on PS2 for like a decade.');
-
-  } else if(answerThree === 'NO' || answerThree === 'N')
-  {
+  } else if(answerThree === 'NO' || answerThree === 'N') {
     alert('That is inCorrect, I have been gaming since the Apple IIe, goto 10 I say...');
-
   } else {
     alert('Please remember to enter: yes  or  no!');
   }
 }
 
-function questionFour(){
-
-
+function questionFour() {
   var questionFour = prompt('Did I work in a California Bar making Burgers?');
   var answerFour = questionFour.toUpperCase().trim();
   console.log(answerFour);
 
-
-  if(answerFour === 'YES' || answerFour === 'Y')
-  {
+  if(answerFour === 'YES' || answerFour === 'Y') {
     alert('That is Correct, Eastbound Bar and Grill and the HighDive in San Diego, however, In and Out Burger, is still my favorite.');
-
-  } else if(answerFour === 'NO' || answerFour === 'N')
-  {
+  } else if(answerFour === 'NO' || answerFour === 'N') {
     alert('That is inCorrect, I have always worked second jobs at pizza places and burger joints.');
-
   } else {
     alert('Please remember to enter: yes  or  no!');
   }
 }
 
-function questionFive(){
-
+function questionFive() {
   var questionFive = prompt('Did I have a paper route, delivering news papers?');
   var answerFive = questionFive.toUpperCase().trim();
   console.log(answerFive);
 
-  if(answerFive === 'YES' || answerFive === 'Y')
-  {
+  if(answerFive === 'YES' || answerFive === 'Y') {
     alert('That is Correct, I was Joliet Herald News Carrier of the Year 1987, I think I was 13.');
-
-  } else if(answerFive === 'NO' || answerFive === 'N')
-  {
+  } else if(answerFive === 'NO' || answerFive === 'N') {
     alert('That is inCorrect, I had 56-60 customers in a 6 block radius and went through three bikes in three years delivering 7 days a week.');
   } else {
     alert('Please remember to enter: yes  or  no!');
@@ -143,7 +108,7 @@ function questionFive(){
 *that we get a 0. But adding the one guarantees that our
 *lowest number will never be lower than 1.*/
 function questionSix(){
-  var y = Math.floor(Math.random() * 10) + 1;
+  var randoNum = Math.floor(Math.random() * 10) + 1;
   var opportunities = 4;
   var robotVoice = 'Guess a integer from 1 to 10!';
 
@@ -159,10 +124,10 @@ function questionSix(){
    argument to a number that represents the object's value. */
     userChoice = Number(userChoice);
     console.log('userChoice is ' + userChoice);
-    console.log('Random generator number is ' + y);
+    console.log('Random generator number is ' + randoNum);
 
     /**check condition of input to random number selected */
-    if (userChoice === y) {
+    if (userChoice === randoNum) {
       alert('thats right you guessed the random number way to go!');
       /**I need help with this part I know I need it I just dont know why. */
       opportunities = 0;
@@ -171,10 +136,10 @@ function questionSix(){
     /*Notfies user that they were wrong and take the input decides if
     * it was higher or lower than the random number in the if statements. */
       robotVoice = 'You were close, but no, try again.';
-      if (userChoice < y){
+      if (userChoice < randoNum){
         robotVoice += ' Too Low!';
       }
-      if (userChoice > y) {
+      if (userChoice > randoNum) {
         robotVoice += ' Too High!';
       }
       /*This decrements the opportunities each time the user enters a try. */
@@ -185,7 +150,7 @@ function questionSix(){
   }
   /*This line of code sits outside the loop so that if the user cancels or fails
 * to guess correct, then they are told what the answer is. */
-  alert('The Random number Generator choose ' + y + ' as it\'s number .');
+  alert('The Random number Generator choose ' + randoNum + ' as it\'s number .');
 
 }
 
@@ -214,4 +179,9 @@ function questionSeven(){
   alert('You answered ' + correctAnswer + ' out of 7 correctly');
 
   alert('Thanks for Playin...');
+  
+  var results = document.getElementById('results');
+  var finalResults = 'You guessed ' + attempts + ' and got ' + correctAnswer + ' correct answers.';
+  results.innerHTML = finalResults;
 }
+
